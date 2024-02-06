707d5d3897acfb360b8807b670e74966b6fda592
24hawa <hawa.nile.24@gmail.com>
2024-02-06 23:11:39 +0800

    Part 4

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+    <title>Death</title>
 </head>
 <body>
 <h1>
 <?php
-echo "Hai Humans!<br>";
-echo "~Welcome to Darkweb~<br>";
-echo "**Carefull**";
+$greets = "Halo";
+$warns = "!LEAVE!";
+echo "$greets Humans!<br>" . '~Welcome to Darkweb~<br>';
+echo '**Carefull**<br>' . "$warns";
 ?>
 </h1>
     
