a9c9162bfed3080fa2cb30d93ec594ba95e035d6
24hawa <hawa.nile.24@gmail.com>
2024-02-08 18:42:54 +0800

    Part 5

--- index.php
+++ index.php
@@ -4,16 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Death</title>
+    <style>
+    body{
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: sans-serif;
+    }
+    </style>
 </head>
 <body>
-<h1>
-<?php
-$greets = "Halo";
-$warns = "!LEAVE!";
-echo "$greets Humans!<br>" . '~Welcome to Darkweb~<br>';
-echo '**Carefull**<br>' . "$warns";
-?>
-</h1>
+    <h1>Recommended Drama</h1>
+
+    <?php
+        $drama = [
+        ['title' => "Hidden Love",
+         'main' => "Zhe Yuan",
+         'releaseYear' => 2023,
+         'playUrl' => "https://examples.com"],
+
+        ['title' => "Love Between Fairy and Devil",
+         'main' => "Esther",
+         'releaseYear' => 2023,
+         'playUrl' => "https://examples.com"],
+
+        ['title' => "Love Like Galaxy",
+         'main' => "Lusi",
+         'releaseYear' => 2022,
+         'playUrl' => "https://examples.com"],
+        
+         ['title' => "My Super Boyfriend",
+         'main' => "Esther",
+         'releaseYear' => 2021,
+         'playUrl' => "https://examples.com"],
+
+        ['title' => "Sword and Fairy",
+         'main' => "Esther",
+         'releaseYear' => 2024,
+         'playUrl' => "https://examples.com"]
+        ];
     
+    function filterByMain($drama, $main){
+        $filteredDrama = [];
+
+        foreach ($drama as $drama){
+            if($drama['main'] === $main){
+             $filteredDrama[] = $drama;
+        }
+         }
+        return $filteredDrama;
+    }
+     ?>
+    
+    <ul>
+            <?php foreach (filterByMain($drama, 'Zhe Yuan') as $drama) : ?>
+            <li>
+                <a href="<?= $drama['playUrl']?>">
+                    <?= $drama['title']; ?> (<?= $drama['releaseYear']; ?>) - By <?=$drama['main']; ?>
+                </a>
+            </li>
+         <?php endforeach; ?>
+
+    </ul>
+
+
+    
+
 </body>
 </html>
